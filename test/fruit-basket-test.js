7d6397114282f66dc645c0e8be50bb06d362a6d0
siweh <sanemadesi@gmail.com>
2021-11-12 15:40:27 +0200

    Fix the database user and password

--- test/fruit-basket-test.js
+++ test/fruit-basket-test.js
@@ -4,7 +4,8 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 const connectionString =
-  process.env.DATABASE_URL || 'postgresql://localhost:5432/my_fruit_basket';
+  process.env.DATABASE_URL ||
+  'postgresql://siwe:fruitdb@localhost:5432/my_fruit_basket';
 
 const pool = new Pool({
   connectionString,
