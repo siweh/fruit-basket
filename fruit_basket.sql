0e26844879259b51d2a8b3dbf766a00647e39dd5
siweh <sanemadesi@gmail.com>
2021-11-16 12:29:23 +0200

    Fixing the travis file

--- fruit_basket.sql
+++ fruit_basket.sql
@@ -1,5 +1,5 @@
 create table fruit_basket(
-    id int not null,
+    id serial not null primary key,
     fruit_type varchar not null,
     fruit_quantity int not null,
     unit_price decimal (10,3) not null
